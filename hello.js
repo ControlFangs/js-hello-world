ea3603c31688ab7de6a207f53708da5c62292248
ControlFangs <163154411+ControlFangs@users.noreply.github.com>
2024-03-13 07:32:06 -0500

    <A fresh start and addition>

--- hello.js
+++ hello.js
@@ -4,14 +4,23 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+
+console.log("A fresh start") 
+console.log ("1+1=2")
+//or to add two numbers together I belive you can do this\\
+const num1 = 1;
+const num2 = 1;
+const sum = num1 + num2; 
+console.log(`The sum of ${num1} and ${num2} is ${sum}.`);
+
+
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
+   1. Change the message that is printed. Done
+   2. Figure out what the parentheses do: its a fuction you put something in the parenthisies and that thing will come out  . Will the code work without them? It won't
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
    4. Remove the semi-colon, ;.
    5. Print a number. (Bonus: Print two numbers added together).
